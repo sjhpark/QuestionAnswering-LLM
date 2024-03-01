316fb035b9ca923f4183c461a45e14f99c100bbb
sjhpark <jonghoon@andrew.cmu.edu>
2024-03-01 14:53:52 -0500

    minor edit

--- ask_llm.py
+++ ask_llm.py
@@ -1,19 +1,15 @@
 from model import get_llm
 
-def get_answer(model, question:str, is_hf:bool):
+def get_answer(model, question:str):
     try:
-        if is_hf:
-            answer = model.invoke(question)
-        else:
-            answer = model(question)
+        answer = model.invoke(question)
         print(f"Answer:\n{answer}")
     except:
         print("No answer found")
 
 if __name__ == "__main__":
     """Ask directly to the LLM model. Could be prone to hallucination."""
-    LLM, config = get_llm()
-    is_hf = config['params']['is_hf']
+    LLM, _ = get_llm()
     while True:
         query = input(f"Type in your question:")
-        get_answer(LLM, query, is_hf)
+        get_answer(LLM, query)
