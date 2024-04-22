9863e557997fc0ac9debd2b0c62f6a3a6dab5878
sjhpark <jonghoon@andrew.cmu.edu>
2024-04-22 03:23:01 -0400

    Add speech-to-text query

--- ask.py
+++ ask.py
@@ -1,22 +1,48 @@
 import argparse
 from build_rag import RAG, CRAG
 from utils import get_answer
+from audio_utils import get_audio, make_listner, transcribe
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Ask questions")
+    parser.add_argument("--transcribe", action="store_true", help="Use speech-to-text via microphone input")
     parser.add_argument("--CRAG", action="store_true", help="Use CRAG")
     args = parser.parse_args()
 
-    if args.CRAG:
-        from utils_CRAG import get_censored_answer
-        print("Using CRAG")
-        app = CRAG()
-        while True:
-            query = input(f"Type in your question:")
-            get_censored_answer(app, query)
+    if args.transcribe: # use a microphone to ask questions
+        print("Using Speech-to-Text")
+        model = make_listner("base")
+        audio_dir="audio/audio.wav"
+
+        if args.CRAG:
+            from utils_CRAG import get_censored_answer
+            print("Using CRAG")
+            app = CRAG()
+            while True:
+                input("Press Enter to ask a question using your microphone...")
+                get_audio(audio_dir)
+                transcript = transcribe(model, audio_dir)
+                get_censored_answer(app, transcript)
+        else:
+            print("Using RAG")
+            qa_chain = RAG()
+            while True:
+                input("Press Enter to ask a question using your microphone...")
+                get_audio(audio_dir)
+                transcript = transcribe(model, audio_dir)
+                get_answer(qa_chain, transcript)
+
     else:
-        print("Using RAG")
-        qa_chain = RAG()
-        while True:
-            query = input(f"Type in your question:")
-            get_answer(qa_chain, query)
+        if args.CRAG:
+            from utils_CRAG import get_censored_answer
+            print("Using CRAG")
+            app = CRAG()
+            while True:
+                query = input(f"Type in your question:")
+                get_censored_answer(app, query)
+        else:
+            print("Using RAG")
+            qa_chain = RAG()
+            while True:
+                query = input(f"Type in your question:")
+                get_answer(qa_chain, query)
---
+++ audio_utils.py
@@ -0,0 +1,59 @@
+import argparse
+import whisper
+import pyaudio
+import wave
+from utils import color_print
+
+def make_listner(size:str):
+    """
+    OpenAI Whisper (https://github.com/openai/whisper)
+    Size 	Parameters 	English-only model 	Multilingual model 	Required VRAM 	Relative speed
+    tiny 	39 M 	    tiny.en 	        tiny 	            ~1 GB 	        ~32x
+    base 	74 M 	    base.en 	        base 	            ~1 GB 	        ~16x
+    small 	244 M 	    small.en 	        small 	            ~2 GB 	        ~6x
+    medium 	769 M 	    medium.en 	        medium 	            ~5 GB 	        ~2x
+    large 	1550 M 	    N/A 	            large 	            ~10 GB 	        1x
+    """
+    return whisper.load_model(size)
+
+def get_audio(audio_dir:str="audio/audio.wav", record_duration:int=10):
+    # record audio from your microphone and save it to a wav file
+    # code reference: https://stackoverflow.com/questions/35344649/reading-input-sound-signal-using-python
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 2
+    RATE = 44100
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    color_print("* Recording... please speak into the microphone", 'yellow')
+
+    frames = []
+    for i in range(0, int(RATE / CHUNK * record_duration)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    print("* done recording")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    wf = wave.open(audio_dir, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+def transcribe(model, audio_dir):
+    speech = model.transcribe(audio_dir)
+    transcript = speech["text"]
+    color_print(f'Transcribed Speech: \n {transcript}', 'yellow')
+    return transcript
